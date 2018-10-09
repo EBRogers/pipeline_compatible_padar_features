de6d664dd7c595534ede13e7bfce3a323a467358
qutang <tqshelly@gmail.com>
2018-10-09 18:41:40 -0400

    v0.2.11
    1. Add common decorators module
    1. Add decorator to run feature or transformation function on accelerometer dataframe

--- padar_features/libs/data_formatting/decorator.py
+++ padar_features/libs/data_formatting/decorator.py
@@ -12,3 +12,12 @@ Date: 07/10/2018
 """
 import pandas as pd
 import numpy as np
+
+
+def apply_on_accelerometer_dataframe(func):
+    def wrapper_func(df, *args, **kwargs):
+        result = df.copy(deep=True)
+        X = result.iloc[:, 1:].values
+        result.iloc[:, 1:] = func(X, *args, **kwargs)
+        return result
+    return wrapper_func
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='padar_features',
-    version='0.2.10',
+    version='0.2.11',
     packages=find_packages(),
     include_package_data=True,
     description='Extension of feature computation to be used in padar package',
